dc0034cf28943c754d3e1e9f3789a617081415b6
chakradhargopalam <chakradhargopalam30@gmail.com>
2024-12-19 12:57:53 +0100

    pub

--- vpc.tf
+++ vpc.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "ibm-vpc" {
 resource "aws_subnet" "ibm-web-sn"{
   vpc_id     = aws_vpc.ibm-vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
   map_public_ip_on_launch = "true"
   tags = {
     Name = "ibm-web-subnet"
