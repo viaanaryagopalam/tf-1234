165d7015a3e64fa2b9035117bad7329b8d1ac6e0
chakradhargopalam <chakradhargopalam30@gmail.com>
2024-12-19 13:18:19 +0100

    private subnet

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "ibm-vpc" {
   }
 }
 
-# subnet
+# public subnet
 resource "aws_subnet" "ibm-web-sn"{
   vpc_id     = aws_vpc.ibm-vpc.id
   cidr_block = "10.0.1.0/24"
@@ -15,5 +15,15 @@ resource "aws_subnet" "ibm-web-sn"{
   map_public_ip_on_launch = "true"
   tags = {
     Name = "ibm-web-subnet"
+  }
+}
+# private subnet
+resource "aws_subnet" "ibm-data-sn"{
+  vpc_id     = aws_vpc.ibm-vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = "ibm-database-subnet"
   }
 }
