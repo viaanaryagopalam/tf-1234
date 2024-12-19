94903d4c7537865a3dcb4c845b3b138f7361a122
chakradhargopalam <chakradhargopalam30@gmail.com>
2024-12-19 12:40:17 +0100

    create public subnet

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "ibm-vpc" {
     Name = "ibm-sc"
   }
 }
+
+# subnet
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
