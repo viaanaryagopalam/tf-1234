ec214d867d7c8c73b1b9ae5dcdc96ed46ee2fcdd
chakradhargopalam <chakradhargopalam30@gmail.com>
2024-12-19 12:40:47 +0100

    create public subnet

--- vpc.tf
+++ vpc.tf
@@ -11,8 +11,9 @@ resource "aws_vpc" "ibm-vpc" {
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
-
+  availability_zone = "us-east-1"
+  map_public_ip_on_launch = "true"
   tags = {
-    Name = "Main"
+    Name = "ibm-web-subnet"
   }
 }
